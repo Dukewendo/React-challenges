0fd6668056448ac9440106d895e6da3be7a9b000
Dukewendo <dommct@yahoo.co.uk>
2023-02-01 20:52:34 +0000

    rendering components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import boxes from './boxes';
-import Box from './Box';
-import './App.css';
-
-
+import React, { useState } from "react";
+import boxes from "./boxes";
+import Box from "./Box";
+import "./App.css";
 
 export default function App() {
+  const [boxItem, setBoxItem] = useState(boxes);
 
-const [boxItem, setBoxItem] = useState(boxes);
+  function toggleState(id) {
+    setBoxItem(prevBoxItem => {
+      return prevBoxItem.map((item) => {
+       return item.id === id ? {...item, on: !item.on} : item
+      })
+    
+    })
+  }
 
+  const elements = boxItem.map((item) => (
+    <Box key={item.id} on={item.on} statechange={toggleState} id={item.id}/>
+  ));
 
-
-const elements = boxItem.map(item => (
-  <Box key={item.id} on={item.on} />
-))
-
-
-return(
-  <main>
-    {elements}
-  </main>
-)
+  return <main>{elements}</main>;
 }
