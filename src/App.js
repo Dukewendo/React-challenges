9283576eae774a07db7b4431f4da399ffb33093f
Dukewendo <dommct@yahoo.co.uk>
2023-02-01 21:16:31 +0000

    changing state- re-write

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import "./App.css";
 export default function App() {
   const [boxItem, setBoxItem] = useState(boxes);
 
-  function toggleState(id) {
-    setBoxItem(prevBoxItem => {
-      return prevBoxItem.map((item) => {
-       return item.id === id ? {...item, on: !item.on} : item
-      })
-    
+function toggleState(id) {
+  setBoxItem(prevBoxItem => {
+    return prevBoxItem.map((item) => {
+      return item.id === id ? {...item, on: !item.on} : item
     })
-  }
+  })
+}
 
   const elements = boxItem.map((item) => (
     <Box key={item.id} on={item.on} statechange={toggleState} id={item.id}/>
